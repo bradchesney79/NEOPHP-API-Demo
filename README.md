376ff794fd4dabbe0852aa86fa13be626965ceda
Brad Chesney <bradchesney79@gmail.com>
2015-06-12 19:01:30 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NEOPHP-API-Demo
 This is the code that pulls from the Yahoo finance service to show how to get the data.
+
+It asks the service for one or more stock symbols and gets JSON back.
+
+We convert the JSON to a PHP Object and away we go. 
---
+++ stocksAPI.php
@@ -0,0 +1,43 @@
+<?php
+
+$clean = array();
+$response = array();
+
+$endPoint = 'https://query.yahooapis.com/v1/public/yql?';
+$queryFront = 'q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%20%28%22';
+
+/*
+$clean['stocks'] = url_encode(
+				  str_replace(
+					',', //needle
+					' ', // this is what replaces the needle in the haystack
+					$_POST['stocks'] // haystack  <-- sent from the user
+				  )
+				);
+*/
+
+$clean['stocks'] = urlencode(str_replace(',',' ',$_GET['stocks']));
+
+ctype_alnum ( string $text )
+
+$queryEnd = '%22%29&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
+
+$curlRequest = curl_init();
+
+
+curl_setopt($curlRequest, CURLOPT_URL, $endPoint . $queryFront . $clean['stocks'] . $queryEnd);
+curl_setopt($curlRequest, CURLOPT_HEADER, 0);
+
+
+$response = curl_exec($curlRequest);
+
+// close cURL resource, and free up system resources
+curl_close($curlRequest);
+
+var_dump(response);
+
+var_dump(json_decode($response,true));
+
+echo json_last_error();
+
+?>
